4f46ea2278afca2d7cc24d8a9458fb4617b8c8aa
Evgeni <Evgeni@Evgenis-MacBook-Air.local>
2024-05-04 00:23:41 +0700

    ascii art

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: eagranat <eagranat@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 11:37:35 by bperez-a          #+#    #+#             */
-/*   Updated: 2024/05/03 17:19:27 by eagranat         ###   ########.fr       */
+/*   Updated: 2024/05/04 00:22:36 by eagranat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 static void	print_welcome_msg(void)
 {
-	ft_putstr_fd("                                        \n", 2);
-	ft_putstr_fd("               ▄█████████▄              \n", 2);
-	ft_putstr_fd("            ▄███████████████▄           \n", 2);
-	ft_putstr_fd("         ██████▀   ██   ▀███████        \n", 2);
-	ft_putstr_fd("      ███   ▀███   ██   ███▀   ███      \n", 2);
-	ft_putstr_fd("     ██████   ██   ██   ██   ██████     \n", 2);
-	ft_putstr_fd("     ██   ██   ██  ██  ██   ██   ██     \n", 2);
-	ft_putstr_fd("    ███    ██  ██  ██  ██  ██    ███    \n", 2);
-	ft_putstr_fd("    ██ ██   ██  █  ██  █  ██   ██ ██    \n", 2);
-	ft_putstr_fd("    ██  ███  ██ ██ ██ ██ ██  ███  ██    \n", 2);
-	ft_putstr_fd("    ██    ██  ██ █ ██ █ ██  ██    ██    \n", 2);
-	ft_putstr_fd("    ████▄   ██ █  █  █  █ ██   ▄████    \n", 2);
-	ft_putstr_fd("       ████   █          █   ████       \n", 2);
-	ft_putstr_fd("          ██                ██          \n", 2);
-	ft_putstr_fd("          ████████▄  ▄████████          \n", 2);
-	ft_putstr_fd("                  ▀██▀                  \n", 2);
-	ft_putstr_fd("           _       _     _          _ _ \n", 2);
-	ft_putstr_fd(" _ __ ___ (_)_ __ (_)___| |__   ___| | |\n", 2);
-	ft_putstr_fd("| '_ ` _ \\| | '_ \\| / __| '_ \\ / _ \\ | |\n", 2);
-	ft_putstr_fd("| | | | | | | | | | \\__ \\ | | |  __/ | |\n", 2);
-	ft_putstr_fd("|_| |_| |_|_|_| |_|_|___/_| |_|\\___|_|_|\n\n", 2);
+    ft_putstr_fd("\033[1;33m                                    \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m             ▄█████████▄            \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m          ▄███████████████▄         \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m       ██████▀   ██   ▀███████      \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m    ███   ▀███   ██   ███▀   ███    \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m   ██████   ██   ██   ██   ██████   \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m   ██   ██   ██  ██  ██   ██   ██   \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m  ███    ██  ██  ██  ██  ██    ███  \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m  ██ ██   ██  █  ██  █  ██   ██ ██  \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m  ██  ███  ██ ██ ██ ██ ██  ███  ██  \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m  ██    ██  ██ █ ██ █ ██  ██    ██  \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m  ████▄   ██ █  █  █  █ ██   ▄████  \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m     ████   █          █   ████     \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m        ██                ██        \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m        ████████▄  ▄████████        \n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m    eagranat    ▀██▀    bperez-a    \n\033[0m", 2);
+    ft_putstr_fd("\033[1;31m           _       _     _          _ _ \n\033[0m", 2);
+    ft_putstr_fd("\033[1;32m _ __ ___ (_)_ __ (_)___| |__   ___| | |\n\033[0m", 2);
+    ft_putstr_fd("\033[1;33m| '_ ` _ \\| | '_ \\| / __| '_ \\ / _ \\ | |\n\033[0m", 2);
+    ft_putstr_fd("\033[1;34m| | | | | | | | | | \\__ \\ | | |  __/ | |\n\033[0m", 2);
+    ft_putstr_fd("\033[1;35m|_| |_| |_|_|_| |_|_|___/_| |_|\\___|_|_|\n\n\033[0m", 2);
 }
 
 t_program	*init_program()
@@ -66,7 +66,7 @@ int	main(int argc, char **argv, char **envp)
 	program->envp = envp;
 	while (1)
 	{
-		program->input = readline("minishell$ ");
+		program->input = readline("\033[1;32mminishell$ \033[0m");
 		//test_display(program);
 		run(program);
 		add_history(program->input);
