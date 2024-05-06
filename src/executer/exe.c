5e8e6cef38fdf43068c94f544a771e234767bc23
GeneArg <yvgeni92@gmail.com>
2024-05-06 13:38:37 +0700

    exe update with builtins

--- src/executer/exe.c
+++ src/executer/exe.c
@@ -3,19 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   exe.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bperez-a <bperez-a@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eagranat <eagranat@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/03 10:25:50 by eagranat          #+#    #+#             */
-/*   Updated: 2024/05/06 09:34:06 by bperez-a         ###   ########.fr       */
+/*   Created: 2024/05/06 11:42:12 by eagranat          #+#    #+#             */
+/*   Updated: 2024/05/06 13:35:12 by eagranat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
 void	free_split(char **split)
 {
@@ -138,122 +133,63 @@ char *find_env_var_value(char **envp, char *var)
 	return (NULL);
 }
 
-void	ft_cd(char *path, t_program *program)
+
+
+
+bool is_builtin(char *cmd) {
+	return !strcmp(cmd, "cd") || !strcmp(cmd, "echo") || !strcmp(cmd, "export") || !strcmp(cmd, "pwd") || !strcmp(cmd, "env") || !strcmp(cmd, "exit");
+}
+
+
+void	ft_echo(char **argv)
 {
-	(void)program;
-	if (chdir(path) == -1)
+	int i;
+
+	i = 1;
+	if (argv[1] && !ft_strncmp(argv[1], "-n", 3))
+		i++;
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (argv[1] && !ft_strncmp(argv[1], "-n", 3))
+		return ;
+	printf("\n");
+}
+
+void	ft_cd(t_program **program, char **argv)
+{
+	char *home;
+	char *oldpwd;
+	char *pwd;
+	char *path;
+	int ret;
+
+	home = find_env_var_value((*program)->envp, "HOME");
+	oldpwd = find_env_var_value((*program)->envp, "OLDPWD");
+	pwd = find_env_var_value((*program)->envp, "PWD");
+	if (!argv[1] || !ft_strncmp(argv[1], "~", 2))
+		path = home;
+	else if (!ft_strncmp(argv[1], "-", 2))
+		path = oldpwd;
+	else
+		path = argv[1];
+	ret = chdir(path);
+	if (ret == -1)
 	{
 		printf("minishell: cd: %s: No such file or directory\n", path);
+		return ;
 	}
-	else
-	{
-		char *cwd = getcwd(NULL, 0);
-		char *oldpwd = ft_strjoin("OLDPWD=", find_env_var_value(program->envp, "PWD"));
-		program->envp[find_env_var(program->envp, "PWD")] = ft_strjoin("PWD=", cwd);
-		program->envp[find_env_var(program->envp, "OLDPWD")] = oldpwd;
-		free(cwd);
-	}
+	ft_export(program, (char *[]){"export", ft_strjoin("OLDPWD=", pwd), NULL});
+	ft_export(program, (char *[]){"export", ft_strjoin("PWD=", getcwd(NULL, 0)), NULL});
 }
 
-char *ft_strip_quotes(char *str)
+void	ft_exit(t_program **program, t_command *current_command)
 {
-	int len = 0;
-	int i = 0;
-	char *new_str;
-	while (str[i])
-	{
-		if (str[i] != '\'' && str[i] != '\"')
-			len++;
-		i++;
-	}
-	new_str = (char *)malloc(sizeof(char) * (len + 1));
-	i = 0;
-	int j = 0;
-
-	while (str[i])
-	{
-		if (str[i] != '\'' && str[i] != '\"')
-		{
-			new_str[j] = str[i];
-			j++;
-		}
-		i++;
-	}
-	new_str[j] = '\0';
-	return (new_str);
-}
-
-// int check_builtins(t_program *program)
-// {
-// 	program->commands->ret = 0;
-// 	if (!check_builts(program))
-// 		return (program->commands->ret);
-// 	else if (!ft_strncmp(program->commands->argv[0], "env", 4))
-// 		env_command(program);
-// 	else if (!ft_strncmp(program->commands->argv[0], "./", 2) ||
-// 			!ft_strncmp(program->commands->argv[0], "../", 3) ||
-// 			!ft_strncmp(program->commands->argv[0], "/", 1))
-// 		bash_command(program);
-// 	else if (!ft_strncmp(program->commands->argv[0], "export", 7) ||
-// 			!ft_strncmp(program->commands->argv[0], "unset", 6))
-// 		program->envp = 
-// }
-
-void execute(t_program **program)
-{
-	t_command *current_command;
-	//int status;
-	int fd[2];
-	int in;
-	int out;
-	char *cmd_path;
-
-	current_command = (*program)->commands;
-	in = 0;
-	while (current_command)
-	{
-		if (current_command->next)
-		{
-			if (pipe(fd) == -1)
-				exit(1);
-		}
-		else
-			fd[1] = 1;
-		if (current_command->redirect_in)
-		{
-			in = open(current_command->redirect_in, O_RDONLY);
-			if (in < 0)
-				exit(1);
-		}
-		if (current_command->redirect_out)
-		{
-			if (current_command->append)
-				out = open(current_command->redirect_out, O_WRONLY | O_CREAT | O_APPEND, 0777);
-			else
-				out = open(current_command->redirect_out, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-			if (out < 0)
-				exit(1);
-		}
-		
-		//check_builtin(program);
-		// if (current_command->heredoc_content)
-		// {
-		// 	// Implement heredoc
-		// }
-		// if (!ft_strncmp(current_command->argv[0], "cd", 3))
-		// {
-		// 	ft_cd(current_command->argv[1], program);
-		// 	current_command = current_command->next;
-		// 	// change directory in pwd by using __environ
-		// 	continue;
-		// }
-		// else if (!ft_strncmp(current_command->argv[0], "unset", 6))
-		// {
-		// 	// Implement unset
-		// }
-		if (!ft_strncmp(current_command->argv[0], "exit", 5))
-		{
-			bool is_digit;
+	bool is_digit;
 			int i;
 
 			ft_putstr_fd("exit\n", 1);
@@ -293,102 +229,140 @@ void execute(t_program **program)
 				}
 				free_program((*program), 0);
 			}
-		}
-	else
+}
+
+void execute_builtin_with_redirection(t_command *cmd, t_program **program, int in_fd, int out_fd) {
+    int saved_stdout = dup(1);
+    int saved_stdin = dup(0);
+
+    if (saved_stdout == -1 || saved_stdin == -1) {
+        perror("Failed to save STDIN or STDOUT");
+        exit(EXIT_FAILURE);
+    }
+
+    dup2(in_fd, 0);
+    dup2(out_fd, 1);
+
+    if (!strcmp(cmd->argv[0], "export"))
+        ft_export(program, cmd->argv);
+	else if (!strcmp(cmd->argv[0], "echo")) 
+        ft_echo(cmd->argv);
+	else if (!ft_strncmp(cmd->argv[0], "pwd", 4))
+		printf("%s\n", getcwd(NULL, 0));
+	else if (!ft_strncmp(cmd->argv[0], "env", 4))
 	{
-		pid_t pid = fork();
-		if (pid == 0)
+		for (int i = 0; (*program)->envp[i]; i++)
 		{
-			// This is the child process
-			if (current_command->next)
-				dup2(fd[1], 1);
-			if (fd[0] != 0)
-				dup2(fd[0], 0);
-			if (in != 0)
-				dup2(in, 0);
-			if (out != 1)
-				dup2(out, 1);
-			if (!ft_strncmp(current_command->argv[0], "env", 4))
-			{
-				for (int i = 0; (*program)->envp[i]; i++)
-				{
-					if (ft_strncmp((*program)->envp[i], "_=", 2))
-						printf("%s\n", (*program)->envp[i]);
-				}
-			}
-			else if (!ft_strncmp(current_command->argv[0], "pwd", 4))
-				printf("%s\n", getcwd(NULL, 0));
-			else if (!ft_strncmp(current_command->argv[0], "echo", 5))
-			{
-				if (current_command->argv[1] && !ft_strncmp(current_command->argv[1], "-n", 3))
-				{
-					for (int i = 2; current_command->argv[i]; i++)
-					{
-						printf("%s", current_command->argv[i]);
-						if (current_command->argv[i + 1])
-							printf(" ");
-					}
-				}
-				else
-				{
-					for (int i = 1; current_command->argv[i]; i++)
-					{
-						printf("%s", current_command->argv[i]);
-						if (current_command->argv[i + 1])
-							printf(" ");
-					}
-					printf("\n");
-				}
-			}
-			else
-			{
-				if (current_command->argv[0][0] == '/')
-					cmd_path = ft_strdup(current_command->argv[0]);
-				else
-					cmd_path = find_path((*program)->envp, current_command->argv[0]);
-				int execstat = execve(cmd_path, current_command->argv, (*program)->envp);
-				if (execstat == -1)
-				{
-					printf("minishell: %s: command not found\n", current_command->argv[0]);
-					free(cmd_path);
-					exit(1);
-				}
-				free(cmd_path);
-			}
-			exit (0);
+			if (ft_strncmp((*program)->envp[i], "_=", 2))
+				printf("%s\n", (*program)->envp[i]);
 		}
-		else if (pid > 0)
-		{
-
-			// This is the parent process
-			if (!ft_strncmp(current_command->argv[0], "export", 7))
-			{
-				ft_export(program, current_command->argv);
-			}
-			// Wait for the child process to finish
-
-			int status;
-			waitpid(pid, &status, 0);
-			
-			
-			if (WIFEXITED(status))
-			{
-				printf("Program exited with status %d\n", WEXITSTATUS(status));
-				ft_export(program, (char *[]){"export", ft_strjoin("?=", ft_itoa(WEXITSTATUS(status))), NULL});
-			}
-			if (WIFSIGNALED(status))
-			{
-				printf("Program was killed by signal %d\n", WTERMSIG(status));
-				printf("Program exited with status %d\n", status);
-				ft_export(program, (char *[]){"export", ft_strjoin("?=", ft_itoa(status + 128)), NULL});
-			}
-		}
-		else
-		{
-			// Fork failed
-			printf("fork failed\n");
-		}
-		current_command = current_command->next;
 	}
+	else if (!strcmp(cmd->argv[0], "cd"))
+		ft_cd(program, cmd->argv);
+	else if (!ft_strncmp(cmd->argv[0], "exit", 5))
+		ft_exit(program, cmd);
+    // Restore STDIN and STDOUT
+    dup2(saved_stdin, 0);
+    dup2(saved_stdout, 1);
+    close(saved_stdin);
+    close(saved_stdout);
 }
+
+void execute_in_child(t_command *cmd, t_program **program, int in_fd, int out_fd) {
+
+	char *cmd_path;
+    pid_t pid = fork();
+    if (pid == 0) {  // Child process
+        if (in_fd != 0) {
+            dup2(in_fd, 0);
+            close(in_fd);
+        }
+        if (out_fd != 1) {
+            dup2(out_fd, 1);
+            close(out_fd);
+        }
+
+		if (cmd->argv[0][0] == '/')
+			cmd_path = ft_strdup(cmd->argv[0]);
+		else
+			cmd_path = find_path((*program)->envp, cmd->argv[0]);
+		int execstat = execve(cmd_path, cmd->argv, (*program)->envp);
+		if (execstat == -1)
+		{
+			printf("minishell: %s: command not found\n", cmd->argv[0]);
+			free(cmd_path);
+			exit(EXIT_FAILURE);
+		}
+		free(cmd_path);
+		exit (0);
+    } 
+	else if (pid > 0) {  // Parent process
+        int status;
+        waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			printf("Program exited with status %d\n", WEXITSTATUS(status));
+			ft_export(program, (char *[]){"export", ft_strjoin("?=", ft_itoa(WEXITSTATUS(status))), NULL});
+		}
+		if (WIFSIGNALED(status))
+		{
+			printf("Program was killed by signal %d\n", WTERMSIG(status));
+			printf("Program exited with status %d\n", status);
+			ft_export(program, (char *[]){"export", ft_strjoin("?=", ft_itoa(status + 128)), NULL});
+		}
+
+        if (in_fd != 0) close(in_fd);
+        if (out_fd != 1) close(out_fd);
+    } else {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
 }
+
+void execute(t_program **program) {
+    t_command *current_command = (*program)->commands;
+    int in_fd = 0;  // Standard input by default
+    int out_fd = 1;  // Standard output by default
+    int pipefds[2];
+
+    while (current_command) {
+        // Handle output redirection
+        if (current_command->redirect_out) {
+            out_fd = open(current_command->redirect_out,
+                          current_command->append ? (O_WRONLY | O_CREAT | O_APPEND) : (O_WRONLY | O_CREAT | O_TRUNC),
+                          0777);
+            if (out_fd < 0) {
+                perror("Failed to open output file for redirection");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // Setup pipe if there is a next command
+        if (current_command->next) {
+            if (pipe(pipefds) == -1) {
+                perror("pipe");
+                exit(1);
+            }
+            out_fd = pipefds[1];
+        }
+
+        // Determine if command is a built-in
+        if (is_builtin(current_command->argv[0])) {
+            execute_builtin_with_redirection(current_command, program, in_fd, out_fd);
+        } else {
+            execute_in_child(current_command, program, in_fd, out_fd);
+        }
+
+        // Manage file descriptors for the next command
+        if (current_command->next) {
+            close(pipefds[1]);  // Close the write end of the pipe in the parent
+            if (in_fd != 0) close(in_fd);  // Close the old in_fd if it was redirected
+            in_fd = pipefds[0];  // Next command reads from here
+        } else if (out_fd != 1) {
+            close(out_fd);
+        }
+
+        current_command = current_command->next;
+    }
+}
+
