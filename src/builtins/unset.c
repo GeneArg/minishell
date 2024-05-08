2f625e4acd938bdf191f2e62893055dad3b51b78
GeneArg <yvgeni92@gmail.com>
2024-05-08 23:34:44 +0700

    fixed some bugs

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,14 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bperez-a <bperez-a@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eagranat <eagranat@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:12:50 by eagranat          #+#    #+#             */
-/*   Updated: 2024/05/07 16:51:28 by bperez-a         ###   ########.fr       */
+/*   Updated: 2024/05/08 23:33:06 by eagranat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+int	is_valid_identifier(char *arg)
+{
+	int i;
+
+	i = 0;
+	if (!ft_isalpha(arg[i]) && arg[i] != '_')
+		return (0);
+	i++;
+	while (arg[i])
+	{
+		if (!ft_isalnum(arg[i]) && arg[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int ft_unset(t_program **program, char **argv)
 {
@@ -23,6 +40,11 @@ int ft_unset(t_program **program, char **argv)
 	i = 1;
 	while (argv[i])
 	{
+		if (!is_valid_identifier(argv[i]))
+		{
+			printf("unset: `%s': not a valid identifier\n", argv[i]);
+			return (1);
+		}
 		env_var = ft_strjoin(argv[i], "=");
 		j = find_env_var((*program)->envp, env_var);
 		if (j != -1)
