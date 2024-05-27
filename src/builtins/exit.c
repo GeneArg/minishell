a54650d3dad39c0480101d7a722ffe418a399c48
Bastien Perez Assael <bperez-a@z1t3c4.42bangkok.com>
2024-05-27 14:17:34 +0700

    normed exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: bperez-a <bperez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 10:36:36 by bperez-a          #+#    #+#             */
-/*   Updated: 2024/05/16 10:48:47 by bperez-a         ###   ########.fr       */
+/*   Updated: 2024/05/27 14:16:57 by bperez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,8 @@ long long	mod_atoi(const char *str)
 
 	result = 0;
 	sign = 1;
-	if (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
+	if ((*str == '-' || *str == '+') && (*str++ == '-'))
+		sign = -1;
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
@@ -34,16 +30,43 @@ long long	mod_atoi(const char *str)
 			else
 				return (0);
 		}
-		if (sign == 1 && result > (9223372036854775807LL - (*str - '0')) / 10)
+		if ((sign == 1 && result > (9223372036854775807LL - (*str - '0')) / 10)
+			|| (sign == -1
+				&& (unsigned long long)result > (9223372036854775808ULL - (*str
+						- '0')) / 10))
 			return (-1);
-		else if (sign == -1
-			&& (unsigned long long)result > (9223372036854775808ULL - (*str
-					- '0')) / 10)
-			return (-1);
-		result = result * 10 + (*str - '0');
-		str++;
+		result = result * 10 + (*str++ - '0');
 	}
 	return (result * sign);
+}
+
+long long	set_exit_code(t_program **program, t_command *current_command,
+		bool is_digit)
+{
+	long long	exit_code;
+
+	if (is_digit)
+	{
+		exit_code = mod_atoi(current_command->argv[1]);
+		if (exit_code == -1)
+		{
+			ft_error(program, "exit", "numeric argument required", -1);
+			exit_code = INVALID_ARGS;
+		}
+		else if (exit_code > 255 || exit_code < -255)
+			exit_code = exit_code % 256;
+	}
+	else
+	{
+		ft_error(program, "exit", "numeric argument required", -1);
+		exit_code = INVALID_ARGS;
+	}
+	if (current_command->argv[1] && current_command->argv[2])
+	{
+		ft_error(program, "exit", "too many arguments", -1);
+		exit_code = FAILURE;
+	}
+	return (exit_code);
 }
 
 void	ft_exit(t_program **program, t_command *current_command)
@@ -64,39 +87,13 @@ void	ft_exit(t_program **program, t_command *current_command)
 			i++;
 		while (current_command->argv[1][i])
 		{
-			if (!ft_isdigit(current_command->argv[1][i]))
+			if (!ft_isdigit(current_command->argv[1][i++]))
 			{
 				is_digit = false;
 				break ;
 			}
-			i++;
 		}
-		if (is_digit)
-		{
-			exit_code = mod_atoi(current_command->argv[1]);
-			if (exit_code == -1)
-			{
-				ft_error(program, "exit", "numeric argument required", -1);
-				exit_code = INVALID_ARGS;
-			}
-			else if (exit_code > 255 || exit_code < -255)
-				exit_code = exit_code % 256;
-		}
-		else
-		{
-			ft_error(program, "exit", "numeric argument required", -1);
-			exit_code = INVALID_ARGS;
-		}
-		i = 0;
-		while (current_command->argv[++i])
-		{
-			if (i > 1)
-			{
-				ft_error(program, "exit", "too many arguments", -1);
-				exit_code = FAILURE;
-				break ;
-			}
-		}
+		exit_code = set_exit_code(program, current_command, is_digit);
 		free_and_exit((*program), exit_code);
 	}
 }
