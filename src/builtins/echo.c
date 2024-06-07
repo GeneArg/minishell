ccba039a02f492e67ea7682f8cce765f77f74aae
GeneArg <yvgeni92@gmail.com>
2024-06-07 12:37:24 +0700

    echo improved to handle multi -n flags

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -12,31 +12,52 @@
 
 #include "../../include/minishell.h"
 
-int	ft_echo(char **argv)
+bool is_all_n_flags(char *arg)
 {
-	int		i;
-	bool	n_option;
+    int j = 2;
+    if (arg[0] != '-' || arg[1] != 'n')
+        return false;
+    while (arg[j] != '\0')
+    {
+        if (arg[j] != 'n')
+            return false;
+        j++;
+    }
+    return true;
+}
 
-	n_option = false;
-	i = 1;
-	while (argv[i] && !ft_strncmp(argv[i], "-n", 2) && ft_strlen(argv[i]) == 2)
-	{
-		n_option = true;
-		i++;
-	}
-	if (!argv[i])
-	{
-		if (!n_option)
-			ft_putstr_fd("\n", 1);
-	}
-	while (argv[i])
-	{
-		ft_putstr_fd(argv[i], 1);
-		if (argv[i + 1])
-			ft_putstr_fd(" ", 1);
-		else if (!argv[i + 1] && !n_option)
-			ft_putstr_fd("\n", 1);
-		i++;
-	}
-	return (0);
+int skip_n_flags(char **argv, int start, bool *n_option)
+{
+    int i = start;
+    while (argv[i] && is_all_n_flags(argv[i]))
+    {
+        *n_option = true;
+        i++;
+    }
+    return i;
+}
+
+void print_arguments(char **argv, int start)
+{
+    int i = start;
+    while (argv[i])
+    {
+        ft_putstr_fd(argv[i], 1);
+        if (argv[i + 1])
+            ft_putstr_fd(" ", 1);
+        i++;
+    }
+}
+
+int ft_echo(char **argv)
+{
+    int i = 1;
+    bool n_option = false;
+
+    i = skip_n_flags(argv, i, &n_option);
+    print_arguments(argv, i);
+    if (!n_option)
+        ft_putstr_fd("\n", 1);
+
+    return 0;
 }
